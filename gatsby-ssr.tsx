fb7c112214fa4d69b782422d78c56688b7d09577
Antoine Giret <ant.giret@gmail.com>
2024-03-08 19:19:27 +0100

    build: add ssr

---
+++ gatsby-ssr.tsx
@@ -0,0 +1,15 @@
+import { ChakraBaseProvider } from '@chakra-ui/react';
+import { WrapPageElementBrowserArgs, WrapRootElementBrowserArgs } from 'gatsby';
+import React from 'react';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+import Layout from './src/layout';
+import theme from './src/theme';
+
+export function wrapPageElement(props: WrapPageElementBrowserArgs) {
+  return <Layout {...props} />;
+}
+
+export function wrapRootElement({ element }: WrapRootElementBrowserArgs) {
+  return <ChakraBaseProvider theme={theme}>{element}</ChakraBaseProvider>;
+}
